97ceaabc05461de2d6ecf688934d9ddfe90bec86
mohsinzafar <muhammadmohsinzafar@gmail.com>
2020-06-10 17:43:35 +0500

    test cases

--- sp17_bse-019/lab/src/test/java/pk/cui/sc/exam/lab/LabTest.java
+++ sp17_bse-019/lab/src/test/java/pk/cui/sc/exam/lab/LabTest.java
@@ -9,34 +9,30 @@ import org.junit.Test;
 
 public class LabTest {
 
-	@Test
+
 	public void TestFarenhiteToCentigrade() {
 		assertEquals(10,Lab.fahrenheitToCentigrade(50));
 	}
 	
-	@Test
+
 	public void TestCentigradeToFarehite() {
 		assertEquals(68,Lab.centigradeToFahrenheit(20));
 	}
 	
-	@Test
+
 	public void TestRemoveSignOfExclemation() {
 		assertEquals("Mohsin",Lab.removeSignOfExclaimation("Mohsin!"));
 	}
-	
-	@Test
+
 	public void TestCurrentDay() {
 		Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
 		assertEquals( calendar.get(Calendar.DAY_OF_MONTH),Lab.getCurrentDay());
 	}
-	
-	@Test
+
 	public void TestCurrentMonth() {
 		Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
 		assertEquals(calendar.get(Calendar.MONTH)+1,Lab.getCurrentMonth());
 	}
-	
-	@Test
 	public void TestCurrentYear() {
 		Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
 		assertEquals(calendar.get(Calendar.YEAR),Lab.getCurrentYear());
